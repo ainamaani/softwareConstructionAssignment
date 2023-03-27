dab5dfefa58ce3c1dacd19b06982e1f6b1e62878
isaacainamaani <aina.isaac2002@gmail.com>
2023-03-28 01:04:47 +0300

    Made structure of class Shape

--- src/Shape.java
+++ src/Shape.java
@@ -1,3 +1,18 @@
-public class Shape {
-    
+public abstract class Shape {
+    //instance variable declaration
+
+
+
+
+    //Getters
+
+    //Abstract methods
+    public abstract double area();
+    public abstract double perimeter();
+
+    //Methods
+    public boolean contains(){
+        return false;
+    }
+
 }
