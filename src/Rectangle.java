59fca3a4f3dd760652580e55be2fb671947e577d
Huzie111 <serunjogi135@gmail.com>
2023-03-30 13:12:26 +0300

    Created rectangle class

---
+++ src/Rectangle.java
@@ -0,0 +1,37 @@
+public class Rectangle extends Shape {
+    private double width;
+    private double length;
+
+    public Rectangle(Posn center, double width, double length) {
+        super(center);
+        this.width = width;
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return this.width;
+    }
+
+    public double getLength() {
+        return this.length;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2 * (this.width + this.length);
+    }
+
+    @Override
+    public double area() {
+        return this.width * this.length;
+    }
+
+    @Override
+    public boolean contains(Posn p) {
+        double x = p.getX();
+        double y = p.getY();
+        double cx = getCenter().getX();
+        double cy = getCenter().getY();
+        return Math.abs(x - cx) < length / 2 && Math.abs(y - cy) < width / 2;
+    }
+}
