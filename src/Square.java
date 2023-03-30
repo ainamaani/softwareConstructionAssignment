49b33bb2e78b5c6d0fdbeac2f2e7402b561f4f38
AineKSandra <ainesandra8@gmail.com>
2023-03-30 19:45:20 +0300

    Added constructor and getter

--- src/Square.java
+++ src/Square.java
@@ -1,2 +1,9 @@
 public class Square {
+    public Square(Posn center, double side) {
+        super(center, side, side);
+    }
+
+    public double getSide() {
+        return getWidth();
+    }
 }
